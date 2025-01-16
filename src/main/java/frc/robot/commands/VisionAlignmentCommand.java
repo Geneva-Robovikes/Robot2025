354e4878d280d379508c7a270c21939d9d5793b3
Geneva Robovikes <robovikes3067@gmail.com>
2025-01-16 10:33:35 -0600

    Implemented PID controller in the vision alignment command

--- src/main/java/frc/robot/commands/VisionAlignmentCommand.java
+++ src/main/java/frc/robot/commands/VisionAlignmentCommand.java
@@ -1,9 +1,11 @@
 package frc.robot.commands;
 
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,10 +17,12 @@ public class VisionAlignmentCommand extends Command {
 
   private final VisionSubsystem subsystem;
   private final SwerveSubsystem swerveSubsystem;
+  private final PIDController controller;
 
   public VisionAlignmentCommand(VisionSubsystem s, SwerveSubsystem ss) {
     subsystem = s;
     swerveSubsystem = ss;
+    controller = new PIDController(Constants.ModuleConstants.kDrivePIDkValue, 0, 0);
 
     addRequirements(subsystem);
   }
@@ -31,6 +35,7 @@ public class VisionAlignmentCommand extends Command {
     Optional<PhotonTrackedTarget> optTarget = subsystem.getTarget();
 
     int targetID;
+    boolean contains;
     ChassisSpeeds chassisSpeeds;
     SwerveModuleState[] moduleStates;
 
@@ -38,33 +43,13 @@ public class VisionAlignmentCommand extends Command {
       PhotonTrackedTarget target = optTarget.get();
 
       targetID = target.getFiducialId();
+      contains = IntStream.of(Constants.VisionConstants.kReefAprilTags).anyMatch(x -> x == targetID);
 
-      for(int x = 0; x < Constants.VisionConstants.kReefAprilTags.length; x++) {
-        if(targetID == Constants.VisionConstants.kReefAprilTags[x]) {
-          if (target.getYaw() > Constants.VisionConstants.kReefYawOffset) {
-            chassisSpeeds = new ChassisSpeeds(0, Constants.VisionConstants.kMaxVisionAlignmentSpeed, 0);
-            moduleStates = Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+      if(contains) {
+        chassisSpeeds = new ChassisSpeeds(0, (Constants.VisionConstants.kMaxVisionAlignmentSpeed * controller.calculate(Constants.VisionConstants.kReefYawOffset, target.getYaw())), 0);
+        moduleStates = Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
 
-            swerveSubsystem.setModuleStates(moduleStates);
-          } else if (target.getYaw() < Constants.VisionConstants.kReefYawOffset) {
-            chassisSpeeds = new ChassisSpeeds(0, -Constants.VisionConstants.kMaxVisionAlignmentSpeed, 0);
-            moduleStates = Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-
-            swerveSubsystem.setModuleStates(moduleStates);
-          }
-
-          if (target.getSkew() > Constants.VisionConstants.kReefDistanceOffset) {
-            chassisSpeeds = new ChassisSpeeds(Constants.VisionConstants.kMaxVisionAlignmentSpeed, 0, 0);
-            moduleStates = Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-
-            swerveSubsystem.setModuleStates(moduleStates);
-          } else if (target.getSkew() < Constants.VisionConstants.kReefDistanceOffset) {
-            chassisSpeeds = new ChassisSpeeds(-Constants.VisionConstants.kMaxVisionAlignmentSpeed, 0, 0);
-            moduleStates = Constants.ModuleConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
-
-            swerveSubsystem.setModuleStates(moduleStates);
-          }
-        }
+        swerveSubsystem.setModuleStates(moduleStates);
       }
     }
   }
