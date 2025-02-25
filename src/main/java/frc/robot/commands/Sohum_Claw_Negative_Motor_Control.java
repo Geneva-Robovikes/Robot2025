f37fc38e8e37b51cea3dabbac0a02efd6ab6eeff
Robovikes <robovikes@geneva304.org>
2025-02-25 16:20:34 -0600

    Made a branch with Sohum's precode

---
+++ src/main/java/frc/robot/commands/Sohum_Claw_Negative_Motor_Control.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/** Add your docs here. */
+
+
+package frc.robot.commands;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.Sohum_Claw_Subsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.SensorUtil;
+
+
+public class Sohum_Claw_Negative_Motor_Control extends Command {
+  private final Sohum_Claw_Subsystem sohum_Claw_Subsystem;
+  /** Example static factory for an autonomous command. */
+  public Sohum_Claw_Negative_Motor_Control(Sohum_Claw_Subsystem sohum_Claw_Subsystem) {
+
+    this.sohum_Claw_Subsystem = sohum_Claw_Subsystem;
+    addRequirements(sohum_Claw_Subsystem);
+  }
+  
+  
+  // initializes the operation?
+  @Override
+  public void initialize() {}
+  
+  // called when something needs to happen?
+  @Override
+  public void execute() {
+    sohum_Claw_Subsystem.Set_Motor_Speed(-0.5);
+
+  }
+  // called when the trigger is stopped
+  @Override
+  public void end(boolean interrupted) {
+    sohum_Claw_Subsystem.Set_Motor_Speed(0);
+  
+  // called when all of the operations are done running in this file?
+  }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+    
+  }
---
+++ src/main/java/frc/robot/subsystems/Sohum_Intake_Subsystem.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+
+public class Sohum_Intake_Subsystem extends SubsystemBase {
+    
+    Talon intake_motor;
+    
+
+    public Sohum_Intake_Subsystem() {
+        // configure channel once it is known
+        intake_motor = new Talon(0);
+        intake_motor.setInverted(true);
+    }
+    
+    
+    public void Set_Motor_Speed(double speed) {
+      intake_motor.set(speed);
+    }
+
+      @Override
+      public void periodic() {
+  
+    // This method will be called once per scheduler run
+  
+  
+      
+  }
+}
