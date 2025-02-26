da7c9ec6f01d95d68bc84124178931709c967fd3
Robovikes <robovikes@geneva304.org>
2025-02-26 15:26:43 -0600

    Moving precode to main

---
+++ src/main/java/frc/robot/commands/Sohum_Elevator_Motor_Control.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/** Add your docs here. */
+
+
+package frc.robot.commands;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Sohum_Elevator_Subsystem;
+
+import com.revrobotics.Rev2mDistanceSensor;
+import com.revrobotics.Rev2mDistanceSensor.Port;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Sohum_Elevator_Subsystem;
+
+
+
+public class Sohum_Elevator_Motor_Control extends Command {
+  private final Sohum_Elevator_Subsystem sohum_Elevator_Subsystem;
+  /** Example static factory for an autonomous command. */
+  public Sohum_Elevator_Motor_Control(Sohum_Elevator_Subsystem sohum_Elevator_Subsystem) {
+
+    this.sohum_Elevator_Subsystem = sohum_Elevator_Subsystem;
+    addRequirements(sohum_Elevator_Subsystem);
+  }
+  
+  private Rev2mDistanceSensor distOnboard; 
+  
+  // initializes the operation?
+  @Override
+  public void initialize() {}
+  
+  // called when something needs to happen?
+  @Override
+  public void execute() {
+    if (distOnboard.getRange() >= 0.5) {
+      sohum_Elevator_Subsystem.Set_Motor_Speed(0.5);
+    }
+  }
+  // called when the trigger is stopped
+  @Override
+  public void end(boolean interrupted) {
+    sohum_Elevator_Subsystem.Set_Motor_Speed(0);
+  
+  // called when all of the operations are done running in this file?
+  }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+    
+  }
---
+++ src/main/java/frc/robot/subsystems/Sohum_Elevator_Subsystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+import frc.robot.RobotContainer;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.Rev2mDistanceSensor;
+import com.revrobotics.Rev2mDistanceSensor.Port;
+
+public class Sohum_Elevator_Subsystem extends SubsystemBase{
+    
+    PWMMotorController elevator_motor_controller;
+   
+    private Rev2mDistanceSensor distOnboard; 
+
+    // change min and max height (in meters) when the dimensions are known
+    
+    
+    
+    // first double is
+    // elevatorSim.createElevatorSystem(DCMotor, double, double, double);
+
+    public Sohum_Elevator_Subsystem() {
+        // configure channel once it is known
+        elevator_motor_controller = new PWMSparkMax(0);
+        distOnboard = new Rev2mDistanceSensor(Port.kOnboard);
+        // how instansiate distance sensor????? distanceSensor = new Port();
+        // This might be needed depending on the direction of the motor
+        // elevator_motor_controller.setInverted(true);
+        
+    }
+    
+    
+    public void Set_Motor_Speed(double speed) {
+      elevator_motor_controller.set(speed);
+    }
+
+
+      @Override
+      public void periodic() {
+  
+    // This method will be called once per scheduler run
+  
+  
+      
+  }
+}
+
+
+
