8b4905470fb2b0595d808a0f255d6061d859a171
Geneva-Robovikes <robovikes@geneva304.org>
2025-02-26 17:06:14 -0600

    Pushing the wade computer code

--- src/main/java/frc/robot/subsystems/Intake_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Intake_Subsystem.java
@@ -1,25 +1,30 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
-import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 
 public class Intake_Subsystem extends SubsystemBase {
     
-    Talon intake_motor;
+    Talon intake_motor_intake;
+    Talon intake_motor_tilt;
     
 
     public Intake_Subsystem() {
         // configure channel once it is known
-        intake_motor = new Talon(0);
-        intake_motor.setInverted(true);
+        intake_motor_intake = new Talon(0);
+        intake_motor_intake.setInverted(false);
+
+        intake_motor_tilt = new Talon(0);
+        intake_motor_intake.setInverted(false);
     }
     
     
-    public void Set_Motor_Speed(double speed) {
-      intake_motor.set(speed);
+    public void setIntakeMotorSpeed(double speed) {
+      intake_motor_intake.set(speed);
+    }
+
+    public void setTiltMotorSpeed(double speed) {
+      intake_motor_tilt.set(speed);
     }
 
       @Override
