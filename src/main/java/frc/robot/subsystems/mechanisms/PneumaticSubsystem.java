e5492a0ed1bad2c75b97c408fd4698403a5dfaad
Geneva-Robovikes <robovikes@geneva304.org>
2025-03-05 06:23:14 -0600

    added a compressor to the pneumatics subsystem

--- src/main/java/frc/robot/subsystems/mechanisms/PneumaticSubsystem.java
+++ src/main/java/frc/robot/subsystems/mechanisms/PneumaticSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.mechanisms;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,11 +15,15 @@ public class PneumaticSubsystem extends SubsystemBase {
   private final DoubleSolenoid solenoidTwo;
   private final DoubleSolenoid solenoidThree;
 
+  private final Compressor compressor;
+
   public PneumaticSubsystem() {
     solenoidZero = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 1);
     solenoidOne = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2, 3);
     solenoidTwo = new DoubleSolenoid(PneumaticsModuleType.REVPH, 4, 5);
     solenoidThree = new DoubleSolenoid(PneumaticsModuleType.REVPH, 6, 7);
+
+    compressor = new Compressor(PneumaticsModuleType.REVPH);
 
     solenoidZero.set(DoubleSolenoid.Value.kReverse);
     solenoidOne.set(DoubleSolenoid.Value.kReverse);
